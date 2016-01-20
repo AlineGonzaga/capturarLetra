77ee9d0b5bd2652503279277e25cd5807d1cda7b
Aline Gonzaga <aline22200@gmail.com>
2016-01-20 12:36:35 -0300

    Testando se branch está Ok.

--- src/capturarLetra/LetraC.java
+++ src/capturarLetra/LetraC.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class LetraC {
 	public static void main(String[] args) {
-		
-	    String nome;
+		//branch feature?
+		String nome;
 	    String []soC = new String[10];
 	    Scanner sc = new Scanner(System.in);
 	    int total = 0;
