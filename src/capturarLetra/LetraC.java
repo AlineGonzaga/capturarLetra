a90dad3800c95e1b8ae4591b2f3fae918f93ad0a
Aline Gonzaga <aline22200@gmail.com>
2016-01-23 15:53:25 -0300

    Fazendo com que o nome seja guardado na primeira posição.

--- src/capturarLetra/LetraC.java
+++ src/capturarLetra/LetraC.java
@@ -6,22 +6,23 @@ public class LetraC {
 	public static void main(String[] args) {
 		//branch feature?
 		String nome;
-	    String []soC = new String[10];
+	    String []soC = new String[5];
 	    Scanner sc = new Scanner(System.in);
 	    int total = 0;
 	    for(int c = 0; c < soC.length; c++ ){
 	        System.out.println(" Digite o seu nome:");
 	        nome = sc.next();
 	        if(nome.charAt(0) == 'C'){
-	        	total++;
 	        	soC[total] = nome;
+	        	total++;
 	        }
 	    }
 	    //teste feito para criar uma nova branch
-	    for(int i = 0; i<=total; i++){
-	    	  if (soC[i] != null){
-	    		  System.out.println(soC[i]);
+	    for(int i = 0; i < total; i++){
+	    		  System.out.println( " Posição " + i + " " + soC[i]);
 	    	  }
-	    }
+	    System.out.println(total);
+	
 	}
-}
+	}
+
